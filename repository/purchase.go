e77219c86accf2361ff30e906ca1e22e34c4338a
Nelson Munthe <nnelson.munthe@gmail.com>
2024-11-18 06:41:19 +0700

    purchase transcation

--- repository/purchase.go
+++ repository/purchase.go
@@ -23,7 +23,10 @@ type PurcaseRepositoryInteface interface {
 }
 
 func (p Purchase) Create(ctx context.Context, purchase entity.CreatePurchase, userId string) (entity.CreatePurchase, error) {
-	// create purchase header
+
+	// Get a Tx for making transaction requests.
+	tx := p.db.Begin()
+
 	purchase_header := entity.Purchase_header{
 		Total_price: float64(purchase.Total_price),
 		Customer_id: userId,
@@ -31,9 +34,10 @@ func (p Purchase) Create(ctx context.Context, purchase entity.CreatePurchase, us
 		Updated_at:  time.Now(),
 	}
 
-	err := p.db.Model(&purchase_header).Save(&purchase_header).Error
+	err := tx.Model(&purchase_header).Save(&purchase_header).Error
 
 	if err != nil {
+		tx.Rollback()
 		return purchase, err
 	}
 
@@ -41,8 +45,39 @@ func (p Purchase) Create(ctx context.Context, purchase entity.CreatePurchase, us
 		purchase.Purchase_line[index].Purchase_header_id = purchase_header.ID
 		purchase.Purchase_line[index].Created_at = time.Now()
 		purchase.Purchase_line[index].Updated_at = time.Now()
+		isUpdateStockFailed := p.UpdateStockProduct(ctx, purchase.Purchase_line[index], tx)
+		if isUpdateStockFailed != nil {
+			tx.Rollback()
+			return purchase, isUpdateStockFailed
+		}
 	}
 
-	err = p.db.WithContext(ctx).Save(&purchase.Purchase_line).Error
+	err = tx.WithContext(ctx).Save(&purchase.Purchase_line).Error
+
+	if err != nil {
+		tx.Rollback()
+		return purchase, err
+	}
+
+	tx.Commit()
 	return purchase, err
+}
+
+func (p Purchase) UpdateStockProduct(ctx context.Context, purchase_line entity.Purchase_line, db *gorm.DB) error {
+	var err error
+	product := entity.Product{}
+
+	err = db.Model(&product).Where("id = ?", purchase_line.Product_id).First(&product).Error
+
+	if err != nil {
+		db.Rollback()
+		return err
+	}
+
+	err = db.Model(&product).WithContext(ctx).
+		Where("id = ?", product.ID).
+		Update("total", product.Total-purchase_line.Quantity).
+		Error
+
+	return err
 }
